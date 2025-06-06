3ed378cfdc1dd87d171e733ff3608b2914d72205
aragosar000 <saraharagon09@gmail.com>
2025-06-06 08:20:39 -0700

    Notes app
    Notes app that will allow users to create, delete, and list notes.

---
+++ amplify/storage/resource.ts
@@ -0,0 +1,12 @@
+//set up the access so that only the person who uploads the image can access.
+import { defineStorage } from "@aws-amplify/backend";
+
+//entity_id as a reserved token that will be replaced with the users' identifier when the file is being uploaded. 
+export const storage = defineStorage({
+  name: "amplifyNotesDrive",
+  access: (allow) => ({
+    "media/{entity_id}/*": [
+      allow.entity("identity").to(["read", "write", "delete"]),
+    ],
+  }),
+});
